89a7b39ffc6446f905e62f35fdb82f98b3faf956
PinTrees <pinsforest@gmail.com>
2020-10-06 16:58:41 +0900

    auto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,35 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
+    indent: 'off',
+    'no-tabs': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': ['error', 'windows'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,107 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="title"></site-title>
+      <v-spacer />
+      <v-btn icon @click="save">
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-group
+          v-for="(item, i) in items"
+          :key="i"
+          v-model="item.active"
+          :prepend-icon="item.icon"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="subItem in item.subItems"
+            :key="subItem.title"
+            :to="subItem.to"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="subItem.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view />
+    </v-content>
+
+    <t-footer :footer="footer"></t-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import SiteTitle from '@/views/site/title.vue';
+import TFooter from '@/components/footer.vue';
+//import SiteMenu from '@/components/menu.vue'
 
 export default {
+  components: { SiteTitle, TFooter /*SiteMenu*/ },
   name: 'App',
-
-  components: {
-    HelloWorld
+  data() {
+    return {
+      drawer: false,
+      item: [],
+      title: 'ssssss',
+      footer: 'footer',
+      items: [
+        {
+          title: 'home',
+          icon: 'mdi-home',
+          active: true,
+          subItems: [
+            {
+              title: 'Dashboard',
+              to: '/',
+            },
+            {
+              title: 'About',
+              to: '/about',
+            },
+          ],
+        },
+        {
+          title: 'about',
+          icon: 'mdi-account',
+          subItems: [
+            {
+              title: 'xxx',
+              to: '/xxx',
+            },
+          ],
+        },
+      ],
+    };
   },
-
-  data: () => ({
-    //
-  })
-}
+  mounted() {
+    console.log(this.$firebase);
+  },
+  methods: {
+    save() {
+      console.log('save sss');
+    },
+  },
+};
 </script>
